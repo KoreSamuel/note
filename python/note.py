77eafd8216e35d9ae9e3947f37ec787a6c98eff9
swust-xiaoj <swustxiaojie@163.com>
2017-05-10 00:46:54 +0800

    add python note

--- python/note.py
+++ python/note.py
@@ -63,4 +63,114 @@ elif age <= 6:
 	print('teenager')
 else:
 	print('nothing')
-# next start http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001431676242561226b32a9ec624505bb8f723d0027b3e7000
+# next start 
+
+# 8 for x in [1,2,3] ; range(5) >>> range(0, 5); range(3,5) >>> range(3,5);
+>>> list(range(5))
+[0, 1, 2, 3, 4]
+>>> list(rane(3, 5))
+[3, 4]
+
+# 9 字典 dict >>> key-value键值对, key为不可变，整数/字符串可以当作key
+>>> someone = {'name': 'xiaoming', 'age': 12, 'sex': 'boy'}
+>>> someone['name']
+'xiaoming'
+>>> someone['weight'] = 50
+>>> someone['weight']
+50
+>>> 'height' in someone
+False
+>>> someone.get('height') # 不存在则retuen None，可指定返回值,eg
+>>> someone.get('height', -1)
+-1
+# 删除某个属性，pop(key) ,return 对应的key的value，eg：
+>>> someone.pop('weight')
+50
+>>> someone
+{'name': 'xiaoming', 'age': 12, 'sex': 'boy'}
+
+# 10 set key的集合，无value，无重复key，创建一个set需要一个list集合作为参数,同dict,不可放入可变对象
+>>> s = set([1, 2, 3, 4]);
+>>> s
+{1, 2, 3, 4}
+>>> ss = set([1,1,1,2,3,4,4]) # 过滤重复key
+>>> ss
+{1, 2, 3, 4}
+# key的添加add()和删除remove()
+>>> s.add(5)
+>>> s
+{1, 2, 3, 4, 5}
+>>> s.remove(4)
+>>> s
+{1, 2, 3, 5}
+# 交集'&'  并集'|'   操作
+>>> s1 = set([1, 2, 4])
+>>> s2 = set([2, 3, 4]);
+>>> s1 & s2
+{2, 4}
+>>> s1 | s2
+{1, 2, 3, 4}
+
+# 11 函数abs() max() int() float() str() bool()
+>>> abs(-3)
+3
+>>> int(12.3)
+12
+>>> str(12.4)
+'12.4'
+>>> bool(1)
+True
+# 定义
+def fn(args):
+	statements # 如果是空函数，可以加语句 pass
+def my_abs(x):
+	if x >= 0:
+		return x
+	else:
+		return -x
+>>> my_abs(5)
+5
+>>> my_abs(-3)
+3
+# 返回多个值：实际是返回一个tuple,可以通过多个变量获取返回值
+def multiReturn(x):
+	return x * x, x + x
+>>> x, y = multiReturn(3)
+>>> x
+9
+>>> y
+6
+# 默认参数,必须指向不可变对象
+def defaultArgs(x, y = 2):
+	return x + y
+>>> defaultArgs(2)
+4
+>>> defaultArgs(2, 3)
+5
+# 可变参数,在内部自动将参数转换成一个tuple
+def addx(*args):
+	sum = 0
+	for n in args:
+		sum = sum + n
+	return sum
+>>> addx(1,2,3,4)
+10
+>>> addx(1,2,3,4,5)
+15
+# 如果有一个已存在的list或者tuple，则调用
+>>> s = [1,2,3]
+>>> addx(*s)
+6
+# 关键字参数：这些关键字参数在函数内部自动组装为一个dict,可以扩充函数，接收调用者传入额外参数
+def person(name, age, **kw):
+	return {'name': name, 'age': age, 'else': kw}
+>>> person('xiaoming', 12)
+{'name': 'xiaoming', 'age': 12, 'else': {}}
+>>> person('xiaohong', 13, height=140)
+{'name': 'xiaohong', 'age': 13, 'else': {'height': 140}}
+# 简便写法
+>>> extra = {'weight': 40, 'height': 140};
+>>> person('xiaogang', 15, **extra)
+{'name': 'xiaogang', 'age': 15, 'else': {'weight': 40, 'height': 140}};
+# 命名关键字参数
+# next start http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001431752945034eb82ac80a3e64b9bb4929b16eeed1eb9000 
