a128f93a45882c8fc1adb93bd263879bdb78d0a2
swust-xiaoj <swustxiaojie@163.com>
2017-12-11 19:55:41 +0800

    redux note

---
+++ redux_react/reducers.js
@@ -0,0 +1,42 @@
+import { combineReducers } from 'redux';
+import { Add_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions';
+
+const { SHOW_ALL } = VisibilityFilters;
+
+function visibilityFilter(state = SHOW_ALL, action) {
+    switch (action.type) {
+        case SET_VISIBILITY_FILTER:
+            return action.filter;
+        default:
+            return state;
+    }
+}
+
+function todos(state = [], action) {
+    switch (action.type) {
+        case Add_TODO:
+            return [
+                ...state,
+                {
+                    text: action.text,
+                    completed: false
+                }
+            ];
+        case TOGGLE_TODO:
+            return [
+                ...state.slice(0, action.index),
+                Object.assign({}, state[action.index], {
+                    completed: true
+                }),
+                ...state.slice(action.index + 1)
+            ];
+        default:
+            return state;
+    }
+}
+
+const todoApp = combineReducers({
+    visibilityFilter,
+    todos
+});
+export default todoApp;
